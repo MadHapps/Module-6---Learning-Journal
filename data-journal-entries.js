6608ad786fe35b137787050d4e3568eac095e4ca
MadHapps <115760696+MadHapps@users.noreply.github.com>
2024-02-02 12:44:00 -0800

    Update - page is now interactive

--- data-journal-entries.js
+++ data-journal-entries.js
@@ -1,6 +1,65 @@
-const journalCard = {
-    img: '',
-    date: '',
-    title: '',
-    summary: ''
-}
+// import { v4 as uuidv4 } from 'https://jspm.dev/uuid'
+
+export const journalEntries = [
+    {
+        img: 'http://unsplash.it/1920/1080/?0',
+        date: '1-22-24',
+        title: 'Post ONE',
+        summary: 'Id quis culpa reprehenderit voluptate ipsum fugiat aute fugiat mollit est non tempor proident consectetur.',
+        content: '',
+        id: ''
+    },
+    {
+        img: 'http://unsplash.it/1920/1080/?1',
+        date: '1-22-24',
+        title: 'Post TWO',
+        summary: 'Id quis culpa reprehenderit voluptate ipsum fugiat aute fugiat mollit est non tempor proident consectetur.',
+        content: '',
+        id: ''
+    },
+    {
+        img: 'http://unsplash.it/1920/1080/?2',
+        date: '1-22-24',
+        title: 'Post THREE',
+        summary: 'Id quis culpa reprehenderit voluptate ipsum fugiat aute fugiat mollit est non tempor proident consectetur.',
+        content: '',
+        id: ''
+    },
+    {
+        img: 'http://unsplash.it/1920/1080/?3',
+        date: '1-22-24',
+        title: 'Post FOUR',
+        summary: 'Id quis culpa reprehenderit voluptate ipsum fugiat aute fugiat mollit est non tempor proident consectetur.',
+        content: '',
+        id: ''
+    },
+    {
+        img: 'http://unsplash.it/1920/1080/?4',
+        date: '1-22-24',
+        title: 'Post FIVE',
+        summary: 'Id quis culpa reprehenderit voluptate ipsum fugiat aute fugiat mollit est non tempor proident consectetur.',
+        content: '',
+        id: ''
+    },
+    {
+        img: 'http://unsplash.it/1920/1080/?5',
+        date: '1-22-24',
+        title: 'Post SIX',
+        summary: 'Id quis culpa reprehenderit voluptate ipsum fugiat aute fugiat mollit est non tempor proident consectetur.',
+        content: '',
+        id: ''
+    },
+    {
+        img: 'http://unsplash.it/1920/1080/?6',
+        date: '1-22-24',
+        title: 'Post SEVEN',
+        summary: 'Id quis culpa reprehenderit voluptate ipsum fugiat aute fugiat mollit est non tempor proident consectetur.',
+        content: '',
+        id: ''
+    },
+]
+
+journalEntries.forEach((entry, index) => {
+    entry.id = index
+    console.log(`${entry.title} ID: ${entry.id}`)
+})
--- index.js
+++ index.js
@@ -1,13 +1,121 @@
+import { journalEntries } from "./data-journal-entries.js"
+import { v4 as uuidv4 } from 'https://jspm.dev/uuid'
+
+const featuredPostEl = document.getElementById('featured-post')
+const frontPageCardDisplayEl = document.getElementById('front-page-card-display')
+const recentPostsDisplayEl = document.getElementById('recent-posts-display')
+const journalEntryPage = document.getElementById('journal-entry-page')
+
+let numberOfViewMore = 3
+
 
 addEventListener('click', (e) => {
+    console.log(e.target.dataset.journalEntryId)
     if (e.target.id === 'home-btn') {
         console.log('home was clicked!')
     }
     else if (e.target.id === 'pg-about-me') {
         console.log('about me was clicked!')
     }
+    else if (e.target.id === 'recent-posts-view-btn') {
+        numberOfViewMore += 2
+        renderJournalCardsInside(recentPostsDisplayEl, numberOfViewMore)
+    }
+    else if (e.target.dataset.journalEntryId) {
+        console.log('journl card clicked!')
+        localStorage.setItem("journalSelectionId", e.target.dataset.journalEntryId)
+    }
 })
 
-function renderJournalCardData() {
+function renderJournalCardsInside(displayElement, numberOfCards) {
+    let html = ''
+    if (displayElement === featuredPostEl) {
+        const firstEntry = journalEntries[0]
+        html += `
+        <a href="pages/journalentry.html" class="featured-post-link">
+            <div class="featured-post_image-wrapper" id="featured-post">
+                <img src="${firstEntry.img}" class="featured-post_image" id="featured-post" data-journal-entry-id="${firstEntry.id}">
+            </div>
+            <div class="featured-post_details" id="featured-post">
+                <p class="date" id="featured-post" data-journal-entry-id="${firstEntry.id}">${firstEntry.date}</p>
+                <h1 id="featured-post" data-journal-entry-id="${firstEntry.id}">${firstEntry.title}</h1>
+                <p class="subtext" id="featured-post" data-journal-entry-id="${firstEntry.id}">${firstEntry.summary}</p>
+            </div>
+        </a>
+        `
+    } else if (displayElement === frontPageCardDisplayEl) {
+        journalEntries.splice(numberOfCards, journalEntries.length).forEach(entry => {
+            html += `
+            <div class="journal-card">
+                <a href="pages/journalentry.html">
+                    <img src=${entry.img}" data-journal-entry-id="${entry.id}">
+                    <div class="journal-card_details">
+                        <p data-journal-entry-id="${entry.id}">${entry.date}</p>
+                        <h2 data-journal-entry-id="${entry.id}">${entry.title}</h2>
+                        <p data-journal-entry-id="${entry.id}">${entry.summary}</p>
+                    </div>
+                </a>
+            </div>
+            `
+        })
+    } else if (displayElement === journalEntryPage) {
+        const journalEntry = journalEntries[localStorage.getItem('journalSelectionId')]
 
+        console.log(journalEntry)
+        html += `
+        <div class="journal-entry-focus">
+            <p>${journalEntry.date}</p>
+            <h1>${journalEntry.title}</h1>
+            <p>${journalEntry.summary}</p>
+        </div>
+        <div class="featured-post_image-wrapper">
+            <img src="${journalEntry.img}" class="featured-post_image">
+        </div>
+        <div class="wat">
+            <h2>How I stay committed to learning</h2>
+            <p>Qui laboris laborum dolor nulla minim. Et duis irure eu cupidatat elit. Id et ea sit adipisicing cupidatat
+                ipsum incididunt eiusmod duis dolor nisi veniam. Tempor ex deserunt velit dolor.</p>
+            <h2>How I got started</h2>
+            <p>Id voluptate officia nisi ut reprehenderit commodo. Sit proident esse ut qui ea sint officia consequat esse
+                nisi exercitation ipsum irure dolor. Eu ea voluptate occaecat qui culpa laboris laborum mollit commodo
+                veniam veniam eu consectetur. Exercitation aliqua anim irure enim eiusmod duis reprehenderit proident sit eu
+                ipsum. Dolor ex cillum minim incididunt qui non Lorem enim dolore dolore. Occaecat eiusmod nostrud nostrud
+                exercitation esse ad ullamco aliquip officia non.</p>
+        </div>
+        `
+    } else if (displayElement === recentPostsDisplayEl) {
+        journalEntries.slice(0, numberOfViewMore).forEach(entry => {
+            html += `
+            <div class="journal-card">
+                <a href="/pages/journalentry.html">
+                    <img src=${entry.img}" data-journal-entry-id="${entry.id}">
+                    <div class="journal-card_details">
+                        <p data-journal-entry-id="${entry.id}">${entry.date}</p>
+                        <h2 data-journal-entry-id="${entry.id}">${entry.title}</h2>
+                        <p data-journal-entry-id="${entry.id}">${entry.summary}</p>
+                    </div>
+                </a>
+            </div>
+            `
+        })
+    }
+
+    displayElement.innerHTML = html
+}
+
+// RENDER DISPLAY ELEMENTS
+
+if (featuredPostEl) {
+    renderJournalCardsInside(featuredPostEl)
+}
+
+if (frontPageCardDisplayEl) {
+    renderJournalCardsInside(frontPageCardDisplayEl, 1)
+} else if (recentPostsDisplayEl) {
+    renderJournalCardsInside(recentPostsDisplayEl)
+}
+
+if (journalEntryPage) {
+    renderJournalCardsInside(journalEntryPage)
+    console.log('wat')
 }
